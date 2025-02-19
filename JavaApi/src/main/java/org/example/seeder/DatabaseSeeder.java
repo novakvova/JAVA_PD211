0402990af74cf12e9ffd4ecec21041d548681a82
novakvova <novakvova@gmail>
2025-02-19 17:01:19 +0200

    Begin working app Seed Database

---
+++ JavaApi/src/main/java/org/example/seeder/DatabaseSeeder.java
@@ -0,0 +1,20 @@
+package org.example.seeder;
+
+import lombok.AllArgsConstructor;
+import org.springframework.boot.CommandLineRunner;
+import org.springframework.stereotype.Component;
+
+@Component
+@AllArgsConstructor
+public class DatabaseSeeder implements CommandLineRunner {
+
+
+    private CategorySeeder categorySeeder;
+    private ProductsSeeder productsSeeder;
+
+    @Override
+    public void run(String... args) throws Exception {
+        categorySeeder.seed();
+        productsSeeder.seed();
+    }
+}
