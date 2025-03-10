634f1f122ba93fcbff253d5e58b3ca3e23b32b50
novakvova <novakvova@gmail>
2025-03-10 15:45:30 +0200

    Begin working app auth

--- JavaApi/src/main/java/org/example/repository/IUserRoleRepository.java
+++ JavaApi/src/main/java/org/example/repository/IUserRoleRepository.java
@@ -1,7 +1,11 @@
 package org.example.repository;
 
+import org.example.entities.UserEntity;
 import org.example.entities.UserRoleEntity;
 import org.springframework.data.jpa.repository.JpaRepository;
 
+import java.util.List;
+
 public interface IUserRoleRepository extends JpaRepository<UserRoleEntity, Long> {
+    List<UserRoleEntity> findByUser(UserEntity User);
 }
